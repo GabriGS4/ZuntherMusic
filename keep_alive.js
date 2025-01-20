0c5a4395383fb2e5d995175783faf84fc364507d
Gabri <gabrielsanchezgarregs4@gmail.com>
2025-01-20 10:42:14 +0100

    cambios en el archivo para ejecutar el servidor

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,11 @@
-var http = require('http');
+const express = require("express");
+const app = express();
 
-http.createServer(function(req, res) {
-    res.write("Estoy en vivo");
-    res.end("Hola bots");
-}).listen(8080);
+// Ruta para verificar que el bot está activo
+app.get("/", (req, res) => {
+    res.send("¡El bot está funcionando!");
+});
+
+// Puerto para el servidor
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Servidor Express ejecutándose en el puerto ${PORT}`));
